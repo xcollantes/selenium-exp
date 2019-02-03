e7f8b45c3dc76ec2f376a437d5a05e7858d2c4ba
xcollantes <xcollantes@users.noreply.github.com>
2019-02-03 00:03:47 -0800

    Added other shit

--- scrape.py
+++ scrape.py
@@ -1,3 +1,19 @@
 #env/bin/python3
 
+from selenium import webdriver as wd
+from selenium.webdriver.common.keys import Keys
 
+dr = wd.Chrome("C:/Users/Xavier/Documents/chromedriver.exe")
+
+dr.get("https://linkedin.com")
+
+fname = dr.find_element_by_name('first')
+fname.send_keys('Xavier')
+
+lname = dr.find_element_by_name('last')
+lname.send_keys('Collantes')
+
+btn = dr.find_element_by_name('search')
+btn.click()
+
+print(dr.current_url)
