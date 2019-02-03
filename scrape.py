3f355668a6716f45af681f3c357f68880de34704
xcollantes <xcollantes@users.noreply.github.com>
2019-02-03 13:54:50 -0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -3,25 +3,30 @@
 from selenium import webdriver as wd
 from selenium.webdriver.chrome.options import Options
 
+PROXY = "172.83.138.45:53281"
+
 options = Options()
 options.headless = True
+options.add_argument('--proxy-server=%s' % PROXY)
 
 dr = wd.Chrome("C:/Users/Xavier/Documents/chromedriver.exe", chrome_options=options)
 
-dr.get("https://linkedin.com")
+dr.get("https://whatismyip.com/")
 
-fname = dr.find_element_by_name('first')
-fname.send_keys('Xavier')
 
-lname = dr.find_element_by_name('last')
-lname.send_keys('Collantes')
 
-btn = dr.find_element_by_name('search')
-btn.click()
+
+#fname = dr.find_element_by_name('first')
+#fname.send_keys('Xavier')
+
+#lname = dr.find_element_by_name('last')
+#lname.send_keys('Collantes')
+
+#btn = dr.find_element_by_name('search')
+#btn.click()
 
 print(dr.current_url)
 print(dr.get_window_size())
-#dr.set_window_size(win_size['width'], win_size['height'])
 dr.set_window_size(2000, 1900)
 
 #dr.maximize_window()
